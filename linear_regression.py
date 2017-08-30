9c3ad4786c54cd51bb0215831d2b2a19f3ef5193
miaozhiyong <lonely_7345@hotmail.com>
2017-08-31 07:41:56 +0800

    logistic regression

--- linear_regression.py
+++ linear_regression.py
@@ -21,16 +21,21 @@ def loss(X,Y):
 def inputs():
     #读取或生成训练数据X及期望输出Y
     #http://people.sc.fsu.edu/~jburkardt/datasets/regression/x09.txt
-    weight_age = [[84,46],[73  20],[65  52],[70  30 ],[76  57],[69  25],[63  28],[72  36],
-                  [79  57],[75  44],[27  24],[89  31],[65  52],[57  23],[],[],[],[],[],[],[],[]]
-    blood_fat_content = []
+    weight_age = [[84, 46], [73, 20], [65, 52], [70, 30], [76, 57],
+                  [69, 25], [63, 28], [72, 36], [79, 57], [75, 44],
+                  [27, 24], [89, 31], [65, 52], [57, 23], [59, 60], 
+                  [69, 48], [60, 34], [79, 51], [75, 50], [82, 34],
+                  [59, 46], [67, 23], [85, 37], [55, 40], [63, 30]]
+    blood_fat_content = [354, 190, 405, 263, 451, 302, 288, 385, 402, 
+                         365, 209, 290, 346, 254, 395, 434, 220, 374, 
+                         308, 220, 311, 181, 274, 303, 244]
     return tf.to_float(weight_age),tf.to_float(blood_fat_content)
 
 def  train(total_loss):
     #依据计算的总损失训练或调整模型参数
     learning_rate = 0.0000001
-    tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
-    return
+    return tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
+    
 
 def  evaluate(sess,X,Y):
     #对训练得到的模型进行评估
@@ -56,11 +61,11 @@ with tf.Session() as sess:
 
     total_loss = loss(X,Y)
     train_op = train(total_loss)
-    coord =  tf.train.Coodinator()
+    coord =  tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess = sess,coord = coord)
 
     #实际训练迭代次数
-    training_steps = 1000
+    training_steps = 100
     for step in range(initial_step,training_steps):
         sess.run(train_op)
     
